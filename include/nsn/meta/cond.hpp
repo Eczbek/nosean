7108073c489ce354c4e0afaa9fd36bea1bbbb926
Eczbek <eczbek.void@gmail.com>
2024-04-18 22:25:28 -0400

    Create `nsn::meta::cond {}`

---
+++ include/nsn/meta/cond.hpp
@@ -0,0 +1,24 @@
+#ifndef NSN_HEADER_META_COND
+#	define NSN_HEADER_META_COND
+
+namespace nsn::detail {
+	template<bool, typename, typename>
+	struct cond;
+
+	template<typename first_, typename second_>
+	struct cond<true, first_, second_> {
+		using type = first_;
+	};
+
+	template<typename first_, typename second_>
+	struct cond<false, first_, second_> {
+		using type = second_;
+	};
+}
+
+namespace nsn::meta {
+	template<bool condition_, typename first_, typename second_>
+	using cond = nsn::detail::cond<condition_, first_, second_>::type;
+}
+
+#endif
