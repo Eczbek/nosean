b7c451f98ab54eec1ab88a72dc459e646b6be73b
STCollier <1samuel3collier@gmail.com>
2025-02-03 00:20:15 -0500

    Everything is broken (some more)

---
+++ build/CMakeFiles/nsn.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nsn.dir/src/config.cpp.o"
+  "CMakeFiles/nsn.dir/src/config.cpp.o.d"
+  "CMakeFiles/nsn.dir/src/main.cpp.o"
+  "CMakeFiles/nsn.dir/src/main.cpp.o.d"
+  "CMakeFiles/nsn.dir/src/read_file.cpp.o"
+  "CMakeFiles/nsn.dir/src/read_file.cpp.o.d"
+  "CMakeFiles/nsn.dir/src/term.cpp.o"
+  "CMakeFiles/nsn.dir/src/term.cpp.o.d"
+  "nsn"
+  "nsn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nsn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
